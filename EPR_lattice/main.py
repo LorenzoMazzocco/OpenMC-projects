62757b696cbd03d10f97b1e0421631e374f89088
Lorenzo Mazzocco <lorenzomazzocco@MacBook-Pro-di-Lorenzo.local>
2021-09-30 02:35:50 +0200

    update EPR

--- EPR_lattice/main.py
+++ EPR_lattice/main.py
@@ -4,6 +4,22 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import math
 
+#================================================================ FUNCTIONS =============================
+
+def plot_radially(radial_data, xlabel, ylabel, title, alpha=0.5):
+    geom_image = mpimg.imread('images/geometry/side_view.ppm')
+    fig, axs = plt.subplots(2,1, sharex=True,sharey=True, figsize=(15,10))
+    fig.suptitle('{}'.format(title))
+    axs[0].plot(np.linspace(-assembly_side/2,assembly_side/2,len(radial_data)),radial_data, 'r-', linewidth=2)
+    axs[1].imshow(geom_image, alpha=alpha, origin='lower', extent=[-assembly_side/2,assembly_side/2,0,1.2*max(radial_data)])
+    axs[1].plot(np.linspace(-assembly_side/2,assembly_side/2,len(radial_data)), radial_data,'r-', linewidth=3)
+    axs[1].xaxis.tick_top()
+    axs[1].xaxis.set_label_position('top')
+    axs[1].set_xlabel('{}'.format(xlabel))
+    axs[1].set_aspect('auto')
+    for ax in axs:
+        ax.set_ylabel('{}'.format(ylabel))
+
 #==================================================== VARIABLES =======================
 
 #fuel variables
@@ -23,13 +39,13 @@ pitch = 1.26
 assembly_side = 17*pitch
 
 #tally variables
-mesh_dimension = 300
+mesh_dimension = 600
 red_mesh_dimension = 300
 energies_dimension = 500
 red_energies_dimension = 10
 
 #settings variables
-neutrons_per_batch = 5000
+neutrons_per_batch = 10000
 
 
 #======================================================================= DEFINE MATERIALS =======================
@@ -123,7 +139,7 @@ outer_universe = openmc.Universe(cells=[all_water_cell])
 #CREATE THE LATTICE
 
 lattice = openmc.RectLattice()
-lattice.lower_left = (-pitch*17/2, -pitch*17/2)
+lattice.lower_left = (-assembly_side/2, -assembly_side/2)
 lattice.pitch = (pitch,pitch)
 lattice.outer = outer_universe
 lattice.universes= [[f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f],
@@ -148,10 +164,10 @@ lattice.universes= [[f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f],
 #create geometry
 
 #boundaries
-left = openmc.XPlane(x0=-17*pitch/2, boundary_type='reflective')
-right = openmc.XPlane(x0=17*pitch/2, boundary_type='reflective')
-bottom = openmc.YPlane(y0=-17*pitch/2, boundary_type='reflective')
-top = openmc.YPlane(y0=17*pitch/2, boundary_type='reflective')
+left = openmc.XPlane(x0=-assembly_side/2, boundary_type='reflective')
+right = openmc.XPlane(x0=assembly_side/2, boundary_type='reflective')
+bottom = openmc.YPlane(y0=-assembly_side/2, boundary_type='reflective')
+top = openmc.YPlane(y0=assembly_side/2, boundary_type='reflective')
 
 main_cell = openmc.Cell(fill=lattice, region=+left & -right & +bottom & -top)
 geometry = openmc.Geometry([main_cell])
@@ -183,3 +199,146 @@ geom_plot_side.filename = 'images/geometry/side_view'
 plots = openmc.Plots([geom_plot_top, geom_plot_side])
 plots.export_to_xml()
 openmc.plot_geometry()
+
+
+#================================================================ DEFINE SETTINGS ===========================
+
+point = openmc.stats.Point((0, 0, 0))
+source = openmc.Source(space=point)
+
+settings = openmc.Settings()
+settings.source = source
+settings.batches = 100
+settings.inactive = 20
+settings.particles = neutrons_per_batch
+
+settings.export_to_xml()
+
+#================================================================ DEFINE TALLIES ===========================
+#FILTERS
+#energy
+energies = np.logspace(np.log10(1e-3), np.log10(20.0e6), energies_dimension+1)
+energy_filter = openmc.EnergyFilter(energies)
+
+
+#particle
+particle_filter = openmc.ParticleFilter('neutron')
+
+#mesh
+mesh = openmc.RegularMesh()
+mesh.dimension = [mesh_dimension, mesh_dimension]
+mesh.lower_left = [-assembly_side/2, -assembly_side/2]
+mesh.upper_right = [assembly_side/2, assembly_side/2]
+mesh_filter = openmc.MeshFilter(mesh)
+
+
+#TALLIES
+tallies = openmc.Tallies()
+
+#total flux spectrum
+t_tot_flux_spec = openmc.Tally(name='total_flux')
+t_tot_flux_spec.filters = [energy_filter, particle_filter]
+t_tot_flux_spec.scores = ['flux']
+tallies.append(t_tot_flux_spec)
+
+#mesh tally (fission, capture, scattering RR)
+t_mesh = openmc.Tally(name='mesh_tally')
+t_mesh.filters = [mesh_filter]
+t_mesh.scores = ['fission', '(n,gamma)', 'elastic', 'fission-q-recoverable']
+tallies.append(t_mesh)
+
+
+tallies.export_to_xml()
+
+#================================================================ RUN SIMULATION ===========================
+openmc.run()
+
+#================================================================ POST PROCESSING ===========================
+
+print('\n\nOpening StatePoint file...')
+sp = openmc.StatePoint('statepoint.100.h5')
+
+print('\nPostprocessing started!')
+
+#total flux spectrum
+tally_tot_flux = sp.get_tally(name='total_flux')
+energy_diff = np.diff(energies)
+
+plt.loglog(energies[:-1], tally_tot_flux.mean[:,0,0])
+plt.xlabel('Energy [eV]')
+plt.ylabel('E*Phi(E) [arbitrary units]')
+plt.grid()
+plt.savefig("images/plots/flux_spectrum.png", dpi=700)
+plt.clf()
+
+
+tally_mesh = sp.get_tally(name='mesh_tally')
+#fission reaction rate
+fission_rr = tally_mesh.get_slice(scores=['fission'])
+fission_rr.mean.shape = (mesh_dimension,mesh_dimension)
+
+plt.imshow(fission_rr.mean, cmap='jet')
+plt.title('Fission Reaction Rate [reaction/s]\nmesh_dim: {} -- neutrons/batch: {}'.format(mesh_dimension,neutrons_per_batch), pad=10)
+plt.colorbar()
+plt.savefig("images/plots/fission_RR.png", dpi=700)
+plt.clf()
+
+#scattering reaction rate
+elastic_rr = tally_mesh.get_slice(scores=['(n,elastic)'])
+elastic_rr.mean.shape = (mesh_dimension,mesh_dimension)
+
+plt.imshow(elastic_rr.mean, cmap='jet')
+plt.title('Elastic Scattering Reaction Rate [reaction/s]\nmesh_dim: {} -- neutrons/batch: {}'.format(mesh_dimension,neutrons_per_batch), pad=10)
+plt.colorbar()
+plt.savefig("images/plots/elastic_RR.png", dpi=700)
+plt.clf()
+
+#capture reaction rate
+capture_rr = tally_mesh.get_slice(scores=['(n,gamma)'])
+capture_rr.mean.shape = (mesh_dimension,mesh_dimension)
+
+plt.imshow(capture_rr.mean, cmap='jet')
+plt.title('Radiative Capture Reaction Rate [reaction/s]\nmesh_dim: {} -- neutrons/batch: {}'.format(mesh_dimension,neutrons_per_batch), pad=10)
+plt.colorbar()
+plt.savefig("images/plots/capture_RR.png", dpi=700)
+plt.clf()
+
+#recoverable energy
+rec_ene = tally_mesh.get_slice(scores=['fission-q-recoverable'])
+rec_ene.mean.shape = (mesh_dimension,mesh_dimension)
+
+plt.imshow(rec_ene.mean, cmap='jet')
+plt.title('Recoverable energy [ev/src]\nmesh_dim: {} -- neutrons/batch: {}'.format(mesh_dimension,neutrons_per_batch), pad=10)
+plt.colorbar()
+plt.savefig("images/plots/recoverable_energy.png", dpi=700)
+plt.clf()
+
+print('\nTop plots completed...')
+
+#RADIAL
+radial_index=int((mesh_dimension/2)-1)
+radial_fission = fission_rr.mean[radial_index,:]
+radial_capture = capture_rr.mean[radial_index,:]
+radial_elastic = elastic_rr.mean[radial_index,:]
+radial_recene = rec_ene.mean[radial_index,:]
+
+
+plot_radially(radial_fission, title='Radial distribution of fission reaction rate\nmesh_dim: {} -- neutrons/batch: {}'.format(mesh_dimension,neutrons_per_batch), xlabel='Radial Distance [cm]', ylabel='Fission reaction rate [1/s-src]')
+plt.savefig("images/radial_plots/fission_rr.png", dpi=700)
+plt.clf()
+
+
+plot_radially(radial_capture, title='Radial distribution of (n,gamma) reaction rate\nmesh_dim: {} -- neutrons/batch: {}'.format(mesh_dimension,neutrons_per_batch), xlabel='Radial Distance [cm]', ylabel='(n,gamma) reaction rate [1/s-src]')
+plt.savefig("images/radial_plots/capture_rr.png", dpi=700)
+plt.clf()
+
+
+plot_radially(radial_elastic, title='Radial distribution of elastic scattering reaction rate\nmesh_dim: {} -- neutrons/batch: {}'.format(mesh_dimension,neutrons_per_batch), xlabel='Radial Distance [cm]', ylabel='Elastic Scattering reaction rate [1/s-src]')
+plt.savefig("images/radial_plots/elastic_rr.png", dpi=700)
+plt.clf()
+
+plot_radially(radial_recene, title='Radial distribution of recoverable energy\nmesh_dim: {} -- neutrons/batch: {}'.format(mesh_dimension,neutrons_per_batch), xlabel='Radial Distance [cm]', ylabel='Recoverable energy [eV/src]')
+plt.savefig("images/radial_plots/recoverable_energy.png", dpi=700)
+plt.clf()
+
+print('\nRadial plots completed...')
